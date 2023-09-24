da7ecee877ca932fe16a4ddd614577d13c31d6b2
Vaibhav Umbarkar <vaibhavumbarkar8485@gmail.com>
2023-09-24 08:04:46 +0530

    v1.2

--- app.js
+++ app.js
@@ -1,92 +1,78 @@
-//Import STD Modules
-import express from 'express';
-import bodyParser from 'body-parser';
-import axios from 'axios';
+//import std modules
+import express  from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
 
-//Set express
+//set express
 const app = express();
 
-//Set body-parser
+//set body-parser
 app.use(bodyParser.urlencoded({extended : true}));
 
-//To make Static Folder Visible
+//static folder visible
 app.use(express.static('public'));
 
-//Set EJS Template
+//set EJS template engine
 app.set('view engine', 'ejs');
 
-//Variables Declration (Weather)
-var temp, description, icon_no, icon_url, humidity, air_pressure, wind_speed, location_name='Pune';  
+//var declaration (use for globally)
+var location;
+var url1;
+var url2;
+var weather_obj;
+var aqi_obj;
+var msg="";
 
-//Variables Declration (AQI)
-var aqi, aqi_text, o3, co, no2;
+//var declaration (use for weather data)
+var location_name;
+var temp;
+var description;
+var icon_no;
+var icon_url;
+var humidity;
+var air_pressure;
+var wind_speed;
 
-//Variables Decleration (Other)
-var msg = "";
-
-
-//Error Page
-app.get('/error', (req, res) => {
-    res.render('error_404');
-});
+//var declaration (use for aqi data)
+var aqi;
+var aqi_text;
+var o3, co, no2;
 
 //Welcome Page
-app.get('/', (req, res) => {
-    res.render('welcome');
-});
+app.get('/', async(req, res) => {
+    
+    location = "Pune";
 
-//Submit
-app.post('/submit', (req, res) => {
-    location_name = req.body.location;
-    res.redirect('/home');
-});
+    try{
+        url1 = 'https://api.openweathermap.org/data/2.5/weather?appid=12373943e2d014434396e51558183738&q='+ location +'&units=metric'
+        weather_obj = await axios.get(url1);
 
-//Home Page
-app.get('/home', async (req, res) => {
-
-    try 
-    {
-        const url1 = 'https://api.openweathermap.org/data/2.5/weather?appid=12373943e2d014434396e51558183738&q='+ location_name +'&units=metric'
-
-        // Use a library like Axios to make the HTTP request
-        const response = await axios.get(url1);
-        
-        // Set Message Empty
-        msg = "";
-
-        // AQI Data found, process data
-        const url2 = 'https://api.openweathermap.org/data/2.5/air_pollution?lat='+ response.data.coord.lat +'&lon=' + response.data.coord.lon + '&appid=12373943e2d014434396e51558183738';
-        
-        // Use a library like Axios to make the HTTP request
-        const aqi_res = await axios.get(url2);
-
-        // Location found, process data
-        console.log("Code from Dev: 200");
+        //AQI Data found, process data
+        url2 = 'https://api.openweathermap.org/data/2.5/air_pollution?lat='+ weather_obj.data.coord.lat +'&lon=' + weather_obj.data.coord.lon + '&appid=12373943e2d014434396e51558183738';
+        aqi_obj = await axios.get(url2);
 
         //Declare into Variable (Weather) 
-        temp = response.data.main.temp;
-        description = response.data.weather[0].description;
-        humidity = response.data.main.humidity;
-        air_pressure = response.data.main.pressure;
-        wind_speed = response.data.wind.speed;
-        location_name = response.data.name;
-        icon_no = response.data.weather[0].icon;
+        temp = weather_obj.data.main.temp;
+        description = weather_obj.data.weather[0].description;
+        humidity = weather_obj.data.main.humidity;
+        air_pressure = weather_obj.data.main.pressure;
+        wind_speed = weather_obj.data.wind.speed;
+        location_name = weather_obj.data.name;
+        icon_no = weather_obj.data.weather[0].icon;
         icon_url = "https://openweathermap.org/img/wn/" + icon_no + "@2x.png"
         
         //Declare into Variable (AQI)
-        aqi = aqi_res.data.list[0].main.aqi;
-        o3 = aqi_res.data.list[0].components.o3;
-        co = aqi_res.data.list[0].components.co;
-        no2 = aqi_res.data.list[0].components.no2;
-    } 
-    
-    catch (error) 
-    {
+        aqi = aqi_obj.data.list[0].main.aqi;
+        o3 = aqi_obj.data.list[0].components.o3;
+        co = aqi_obj.data.list[0].components.co;
+        no2 = aqi_obj.data.list[0].components.no2;
+
+    }catch(error){
+
         if (error.code === 'ENOTFOUND')
         {
             // Handle internet connection issue
             console.log('Please check the internet connection!');
-            // res.redirect('/error');
         }
 
         else if(error.code === 'ERR_BAD_REQUEST')
@@ -113,11 +99,81 @@ app.get('/home', async (req, res) => {
         {
             // Handle other errors
             console.error('An error occurred:', error.message);
-            // res.redirect('/error');
         }
     }
 
+    res.render('welcome');
+});
 
+//User Input Req. Handle
+app.post('/submit', async(req, res) => {
+    
+   try{
+        url1 = 'https://api.openweathermap.org/data/2.5/weather?appid=12373943e2d014434396e51558183738&q='+ req.body.location +'&units=metric';
+        weather_obj = await axios.get(url1);
+
+        url2 = 'https://api.openweathermap.org/data/2.5/air_pollution?lat='+ weather_obj.data.coord.lat +'&lon=' + weather_obj.data.coord.lon + '&appid=12373943e2d014434396e51558183738';
+        aqi_obj = await axios.get(url2);
+
+        msg="";
+
+        //Declare into Variable (Weather) 
+        temp = weather_obj.data.main.temp;
+        description = weather_obj.data.weather[0].description;
+        humidity = weather_obj.data.main.humidity;
+        air_pressure = weather_obj.data.main.pressure;
+        wind_speed = weather_obj.data.wind.speed;
+        location_name = weather_obj.data.name;
+        icon_no = weather_obj.data.weather[0].icon;
+        icon_url = "https://openweathermap.org/img/wn/" + icon_no + "@2x.png"
+        
+        //Declare into Variable (AQI)
+        aqi = aqi_obj.data.list[0].main.aqi;
+        o3 = aqi_obj.data.list[0].components.o3;
+        co = aqi_obj.data.list[0].components.co;
+        no2 = aqi_obj.data.list[0].components.no2;
+
+   }catch(error){
+         if (error.code === 'ENOTFOUND')
+        {
+            // Handle internet connection issue
+            console.log('Please check the internet connection!');
+        }
+
+        else if(error.code === 'ERR_BAD_REQUEST')
+        {
+            msg = "Location Not Found!";
+
+            //Set value if location are not found (Weather)
+            temp='NA';
+            description = 'NA';
+            humidity = 'NA';
+            air_pressure = 'NA'; 
+            wind_speed = 'NA';
+            icon_url = 'NA'
+
+            //Set value if location are not found (AQI)
+            aqi='NA';
+            aqi_text='NA'; 
+            o3='NA'; 
+            co='NA';
+            no2='NA';
+        }
+
+        else 
+        {
+            // Handle other errors
+            console.error('An error occurred:', error.message);
+        }
+
+   }
+
+    res.redirect('/home');
+});
+
+//Home Page
+app.get('/home', (req, res) => {
+    
     //AQI Description
     switch(aqi)
     {
@@ -162,10 +218,16 @@ app.get('/home', async (req, res) => {
         msg : msg
 
     });
+})
+
+//Page Not Found
+app.get('/error', (req, res) => {
+    res.render('error_404');
 });
 
 //Local Port 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server Running at : http://localhost:${port}`);
+    // console.log(`Server Running at : http://localhost:${port}`);
+    console.log("Server is Online...");
 });
