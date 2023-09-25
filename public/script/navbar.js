0e057e6af042506e56f660a0e3335c9b15056297
Vaibhav Umbarkar <vaibhavumbarkar8485@gmail.com>
2023-09-25 19:25:20 +0530

    v1.3

---
+++ public/script/navbar.js
@@ -0,0 +1,5 @@
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("navbar_div1").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
